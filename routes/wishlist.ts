d834ef056461094b65694c8a94ba6a90a7c47737
glitcher93 <nigeljdsouza@gmail.com>
2022-04-16 18:35:17 -0400

    changed commonjs to es6 modules

---
+++ routes/wishlist.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import auth from '../middleware/auth'
+import { getWishlist, postNewItem, getWishlistItem, updateWishlistItem, deleteItem } from '../controllers/wishlistController'
+
+const router = express.Router()
+
+router.route('/')
+    .get(auth, getWishlist)
+    .post(auth, postNewItem)
+
+router
+    .get('/:itemId', auth, getWishlistItem)
+
+router
+    .put('/:itemId/update', auth, updateWishlistItem)
+
+router
+    .delete('/:itemId', auth, deleteItem);
+
+module.exports = router;
